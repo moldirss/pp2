7f9cd293cb1f1ce44621476ec04428a777b1017b
moldirss <m_sailaubay@kbtu.kz>
2024-09-15 21:49:53 +0500

    Add files via upload

---
+++ lab1/variables/Variables.py
@@ -0,0 +1,20 @@
+#A variable is created the moment you first assign a value to it.
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#Variables do not need to be declared with any particular type, and can even change type after they have been set.
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#String variables can be declared either by using single or double quotes:
+x = "John"
+#is the same as
+x = 'John'
+
+#Variable names are case-sensitive.
+a = 4
+A = "Sally"
+#A will not overwrite a
