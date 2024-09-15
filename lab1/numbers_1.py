a69a5541c3e363c72b05706108f86b12f9be628f
moldirss <m_sailaubay@kbtu.kz>
2024-09-15 22:07:17 +0500

    Add files via upload

---
+++ lab1/numbers_1.py
@@ -0,0 +1,27 @@
+#Variables of numeric types are created when you assign a value to them:
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#To verify the type of any object in Python, use the type() function:
+print(type(x))
+print(type(y))
+print(type(z))
+
+#You can convert from one type to another with the int(), float(), and complex() methods:
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+#convert from int to float:
+a = float(x)
+#convert from float to int:
+b = int(y)
+#convert from int to complex:
+c = complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+
