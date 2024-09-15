e26b492679c4280edb6d409e64b6065491025ffa
moldirss <m_sailaubay@kbtu.kz>
2024-09-15 22:03:19 +0500

    Add files via upload

---
+++ lab1/datatypes.py
@@ -0,0 +1,18 @@
+#You can get the data type of any object by using the type() function:
+x = 5
+print(type(x))
+
+x = str("Hello World")	
+x = int(20)	
+x = float(20.5)	
+x = complex(1j)	
+x = list(("apple", "banana", "cherry"))	
+x = tuple(("apple", "banana", "cherry"))	
+x = range(6)	
+x = dict(name="John", age=36)	
+x = set(("apple", "banana", "cherry"))	
+x = frozenset(("apple", "banana", "cherry"))	
+x = bool(5)		
+x = bytes(5)	
+x = bytearray(5)	
+x = memoryview(bytes(5))
