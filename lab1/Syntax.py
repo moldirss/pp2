d5dc775c65ce3b632d9b6e07ece9e10bf2b14d3c
moldirss <m_sailaubay@kbtu.kz>
2024-09-15 21:44:46 +0500

    Add files via upload

---
+++ lab1/Syntax.py
@@ -0,0 +1,20 @@
+#Indentation refers to the spaces at the beginning of a code line.
+#Python will give you an error if you skip the indentation:
+
+if 5 > 2:
+    print("Five is greater than two!")
+if 5 > 2:
+    print("Five is greater than two!")
+    
+#Python will give you an error if you skip the indentation:
+"""
+if 5 > 2:
+print("Five is greater than two!")
+"""
+
+#The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+if 5 > 2:
+ print("Five is greater than two!")  
+if 5 > 2:
+        print("Five is greater than two!") 
+        
