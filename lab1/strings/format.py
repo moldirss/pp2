91a6706902e48ce023a1b85a67cb556fc59f99d0
moldirss <m_sailaubay@kbtu.kz>
2024-09-18 10:42:44 +0500

    Update format.py

--- lab1/strings/format.py
+++ lab1/strings/format.py
@@ -7,12 +7,3 @@ print(txt)
 price = 59
 txt = f"The price is {price} dollars" 
 print(txt)
-
-#A placeholder can include a modifier to format the value
-price = 59
-txt = f"The price is {price:.2f} dollars"
-print(txt)
-
-#Perform a math operation in the placeholder
-txt = f"The price is {59 * 20} dollars"
-print(txt)
