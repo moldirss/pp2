f88c325208290c992e9d14d37867a55df782cf3b
moldirss <m_sailaubay@kbtu.kz>
2024-09-25 00:09:38 +0500

    Add files via upload

---
+++ lab2/booleans.py
@@ -0,0 +1,13 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+#It will return true or false
+#When you run a condition in an if statement, Python returns True or False:
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
---
+++ lab2/operators.py
@@ -0,0 +1,5 @@
+#Operators are used to perform operations on variables and values.
+print(10 + 5)
+print(100 + 5 * 3)
+#Addition + and subtraction - has the same precedence, and therefor we evaluate the expression from left to right:
+print(5 + 4 - 7 + 3)
